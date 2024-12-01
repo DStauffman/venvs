f10b00490eef413f5a07275baa3f3cdf654d71da
David Stauffer <dstauffman@yahoo.com>
2024-11-30 21:01:44 -0800

    Remove some dead code

--- make_python_environments.bat
+++ make_python_environments.bat
@@ -2,6 +2,8 @@ cd C:\Users\%username%\Documents\venvs
 
 set "py_version=3.12"
 
+py -%py_version% -m pip install --upgrade pip
+
 rmdir /S /Q core_only
 py -%py_version% -m venv --upgrade-deps core_only
 call .\core_only\Scripts\activate.bat
@@ -12,19 +14,15 @@ call deactivate.bat
 rmdir /S /Q static
 py -%py_version% -m venv --upgrade-deps static
 call .\static\Scripts\activate.bat
-REM coverage[toml] is no longer needed, as tomllib is now core python
-REM pprofile is no longer supported, find different option to profile with
 python -m pip install pip wheel
-python -m pip install black coverage flake8 h5py matplotlib mypy numpy openpyxl pandas pyarrow pylint PyQt5 PyQt5-Stubs pyright pytest pytest-cov qtpy types-openpyxl types-python-dateutil scipy tblib urllib3
+python -m pip install black coverage flake8 h5py matplotlib mypy numpy openpyxl pandas pprofile pyarrow pylint PyQt5 PyQt5-Stubs pyright pytest pytest-cov qtpy types-openpyxl types-python-dateutil scipy tblib urllib3
 call deactivate.bat
 
 rmdir /S /Q everything
 py -%py_version% -m venv --upgrade-deps everything
 call .\everything\Scripts\activate.bat
-REM pprofile is no longer supported, find different option to profile with
 python -m pip install pip wheel
-python -m pip install coverage datashader h5py matplotlib mypy numba numpy openpyxl pandas pyarrow PyQt5 pyqtgraph pytest pytest-cov qtpy scipy spyder-kernels tblib urllib3
-REM python -m pip install dask[dataframe]
+python -m pip install coverage dask[dataframe] datashader h5py matplotlib mypy numba numpy openpyxl pandas pprofile pyarrow PyPDF2 PyQt5 pyqtgraph pytest pytest-cov qtpy scipy spyder-kernels tblib urllib3
 call deactivate.bat
 
 rmdir /S /Q spyder
@@ -40,16 +38,5 @@ rmdir /S /Q aiml
 py -%py_version% -m venv --upgrade-deps aiml
 call .\aiml\Scripts\activate.bat
 python -m pip install pip wheel
-python -m pip install coverage dask[dataframe] datashader h5py matplotlib mypy numba numpy openpyxl pandas pyarrow PyQt5 pyqtgraph pytest pytest-cov qtpy scikit-learn scipy seaborn spyder-kernels tblib torch torchvision torchaudio urllib3
-python -m pip install https://storage.googleapis.com/tensorflow/versions/2.16.1/tensorflow-2.16.1-cp312-cp312-win_amd64.whl
+python -m pip install coverage dask[dataframe] datashader graphviz h5py jax matplotlib mypy numba numpy openpyxl pandas pyarrow pydot PyQt5 pyqtgraph pytest pytest-cov qtpy scikit-learn scipy seaborn spyder-kernels tblib tensorflow torch torchvision torchaudio urllib3
 call deactivate.bat
-
-REM rmdir /S /Q poetry_env
-REM py -%py_version% -m venv --upgrade-deps poetry_env
-REM call .\poetry_env\Scripts\activate.bat
-REM python -m pip install pip wheel
-REM python -m pip install poetry
-REM poetry config virtualenvs.create true
-REM poetry config virtualenvs.in-project
-REM poetry config virtualenvs.path "C:\Users\%username%\Documents\venvs\poetry_dstauffman"
-REM call deactivate.bat
